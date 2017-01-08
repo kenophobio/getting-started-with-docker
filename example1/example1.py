dc5df368702738e14e4352332ee3b31bc837f766
Guyon Moree <guyon.moree@gmail.com>
2017-01-08 18:24:03 +0100

    updated examples

--- example1/example1.py
+++ example1/example1.py
@@ -1,10 +1,14 @@
-# example1/example1.py
+python
+# getting-started-with-docker/example1/example1.py
 from itertools import islice
 
 import click
 
 
 def fibonacci_sequence():
+    """
+    Fibbonacci sequence generator
+    """
     i, j = 0, 1
     while True:
         yield i
@@ -12,11 +16,13 @@ def fibonacci_sequence():
 
 
 @click.command()
-@click.option('--length', default=5, help='Length of the Fibonacci sequence.')
+@click.option('--length', default=5)
 def example1(length):
-    """Simple program that prints out LENGTH items of the Fibonacci sequence."""
+    """
+    Simple program that prints out the Fibonacci sequence.
+    """
     for i in islice(fibonacci_sequence(), length):
-        click.echo('--> {}'.format(i))
+        click.echo('-> {}'.format(i))
 
 
 if __name__ == '__main__':
